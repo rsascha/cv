3aa99588b312d4ac26ebe400bfb59f7ff6e52a78
Sascha Rose <sascha.rose@gmail.com>
2025-07-21 12:45:30 +0200

    technologies layout, text updates

--- packages/web/src/components/Technologies.tsx
+++ packages/web/src/components/Technologies.tsx
@@ -5,11 +5,9 @@ type TechStackItemProps = {
 
 export function TechStackItem({ technology, logo }: TechStackItemProps) {
   return (
-    <div className="flex flex-col w-20">
-      <div>
-        <img src={logo} alt={`${technology} logo`} className="w-11 h-11" />
-      </div>
-      <div>{technology}</div>
+    <div className="flex flex-col mb-1 items-center flex-1/2">
+      <img src={logo} alt={`${technology} logo`} className="w-12 h-12" />
+      <div className="text-xs mt-1 text-center">{technology}</div>
     </div>
   );
 }
@@ -18,19 +16,18 @@ export function Technologies({ className = "" }: { className?: string }) {
   return (
     <div className={className}>
       <h2>My Core Stack</h2>
-      <div className={"flex flex-wrap justify-between gap-4"}>
+      <div className={"flex flex-wrap justify-between"}>
         <TechStackItem technology="React" logo="react.png" />
         <TechStackItem technology="React Native" logo="phone.png" />
         <TechStackItem technology="HTML" logo="html5.png" />
         <TechStackItem technology="CSS" logo="css3.png" />
         <TechStackItem technology="Tailwind CSS" logo="tailwind.png" />
 
-        <TechStackItem technology="JavaScript" logo="js.png" />
         <TechStackItem technology="TypeScript" logo="ts.png" />
+        <TechStackItem technology="JavaScript" logo="js.png" />
         <TechStackItem technology="Python" logo="python.png" />
 
-        <TechStackItem technology="Node.js" logo="nodejs.png" />
-        <TechStackItem technology="Express.js" logo="express.png" />
+        <TechStackItem technology="Node.js (Express)" logo="nodejs.png" />
         <TechStackItem technology="PostgreSQL" logo="pg.png" />
         <TechStackItem technology="MongoDB" logo="mongo.png" />
 
